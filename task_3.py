8d4e8d450c9e01323e763ece98be5838f4c3e044
MeganJard <57149105+MeganJard@users.noreply.github.com>
2020-10-29 17:33:31 +0300

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,13 @@
+def is_correct(*args):
+    answer = 0.
+    errs_conter = 0
+    for i in args:
+        data = open(i).readlines()
+        for j in data:
+            try:
+                answer += sum([1 / float(k) for k in j.split()])
+            except Exception:
+                errs_conter += 1
+                break
+    return (answer, errs_conter)
+
